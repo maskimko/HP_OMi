240b5000806a61b442836961a3e693f795721d34
maskimko <maskimko@murcielago.sdab.sn>
2013-11-12 16:02:41 +0200

    Before prod Fix description truncating

---
+++ OMi/src/Cutter.groovy
@@ -0,0 +1,18 @@
+
+class Cutter {
+
+	
+	public static void main(String[] args){
+		String toCut = "Test Incident ;lkjfa;slkjfdporewutwperjt;lsakjd;flnsa/v.,ncxz/.vm;'lkdj;lsjg;fdsugpwoir";
+		int len = 15;
+		if (toCut != null) {
+			if (toCut.length() > len) {
+				toCut = toCut.substring(0,len);
+			}
+		} else {
+			toCut = "";
+		}
+		System.out.println(toCut.length());
+		System.out.println(toCut);
+	}
+}
